fb40c50aa02bdcd48e0e913d77ec1bc650e789ff
caoxiaolan <caoxiaolan@mioji.com>
2017-09-27 18:07:26 +0800

    modify cache.py

--- PPViewWidget/web/server.py
+++
@@ -1,5 +0,0 @@
-from CGIHTTPServer import CGIHTTPRequestHandler 
-from BaseHTTPServer import HTTPServer 
-server_address=('',8000) 
-httpd = HTTPServer(server_address, CGIHTTPRequestHandler)
-httpd.serve_forever()
--- cache.py
+++ cache.py
@@ -12,72 +12,54 @@ import sys
 import optparse
 import io
 import urllib2
+from DBHandle import DBHandle
 reload(sys);
 exec("sys.setdefaultencoding('utf-8')");
 
-qid_dic=dict()
-
 def Report():
-    global qid_dic
-    print "global"
+    qid_dic=dict()
     dbb="logQuery_%s"%kEnv
-    conn=MySQLdb.connect(host='10.10.107.246',user='reader',passwd='miaoji1109',db=dbb,port=3306, charset="utf8")
-    print dbb
-    cur=conn.cursor()
-    try:
-        sqlstr="select req_params from nginx_api_log_%s where qid=%s and log_type='13' and req_type in('s125','s127','s128','s129' ,'s130' ,'p101' ,'p104' ,'p105','b116');"%(kDate,kQid)
-        print sqlstr
-        cur.execute(sqlstr)
-        conn.commit()
-        row=cur.fetchone()
-        qid_dic[kQid]=dict()
-        qid_dic[kQid]["req"]=row
-        print "row=%s"%row
-        print"in select \n"
-        sqlstr="select id from nginx_api_log_%s where qid=%s and log_type='14' and req_type in('s125' ,'s127','s128','s129','s130' ,'p101','p104','p105' ,'b116');"%(kDate,kQid)
-        print sqlstr
-        cur.execute(sqlstr)
-        conn.commit()
-        ids=cur.fetchone()
-        print "ids=%s"%ids
-        sqlstr="select response from nginx_api_resp_%s where id=%s;"%(kDate,ids[0])
-        print sqlstr
-        cur.execute(sqlstr)
-        conn.commit()
-        resp=cur.fetchone()
-        qid_dic[kQid]["resp"]=resp
-    except (MySQLdb.Error) as e:
-        print'Got error {!r}, errno is {}'.format(e, e.args[0])
-    cur.close()
-    conn.close()
-
-    conn=MySQLdb.connect(host='127.0.0.1',user='root',passwd='',db='test',port=3306, charset="utf8")
-    cur=conn.cursor()
-    try:
-        sqlstr="replace into error_qid values(%(env)s,%(date)s,%(qid)s,%(req)s,%(resp)s)"
-        T={"env":kEnv,"date":kDate,"qid":kQid,"req":qid_dic[kQid]["req"],"resp":qid_dic[kQid]["resp"]}
-        cur.execute(sqlstr,T)
-        conn.commit()
-    except (MySQLdb.Error) as e:
-        print 'Got error {!r},errno is{}'.format(e,e.args[0])
-
-    cur.close()
-    conn.close()
+    conn=DBHandle('10.10.107.246','reader','miaoji1109',dbb)
+    sqlstr="select req_params from nginx_api_log_%s where qid=%s and log_type='13' and req_type='%s';"%(kDate,kQid,kType)
+    req=conn.do(sqlstr)
+    if len(req)==0:
+        print "no req"
+        return
+    qid_dic[kQid]=dict()
+    qid_dic[kQid]["req"]=req[0]["req_params"]
+    sqlstr="select id from nginx_api_log_%s where qid=%s and log_type='14' and req_type='%s';"%(kDate,kQid,kType)
+    ids=conn.do(sqlstr)
+    sqlstr="select response from nginx_api_resp_%s where id=%s;"%(kDate,ids[0]['id'])
+    resp=conn.do(sqlstr)
+    if len(resp)==0:
+        print "no resp"
+        return
+    qid_dic[kQid]["resp"]=resp[0]['response']
+    conn=DBHandle('10.10.169.10','root','miaoji1109','viewdiff')
+    sqlstr="replace into error_qid (env,date,type,qid,req,resp) values(%s,%s,%s,%s,%s,%s);"
+    args=list()
+    T=(kEnv,kDate,kType,kQid,qid_dic[kQid]["req"],qid_dic[kQid]["resp"])
+    args.append(T)
+    conn.do(sqlstr,args)
 
 
-kQid=1504503138335
-kEnv="online"
+kQid=''
+kEnv=''
+kDate=''
+kType=''
 def main():
     global kDate
     global kQid
     global kEnv
+    global kType
     parser = optparse.OptionParser()
-    parser.add_option('-q', '--qid', help = 'run qid like 1504490041011')
-    parser.add_option('-e', '--env', type="string",default="online",help = 'run env like online')
+    parser.add_option('-q', '--qid' )
+    parser.add_option('-t', '--type' )
+    parser.add_option('-e', '--env', type="string",default="test",help = 'run env like test')
     opt, args = parser.parse_args()
     tt=int(opt.qid)
     kDate=time.strftime("%Y%m%d",time.localtime(tt/1000))
-    print kDate
+#    print kDate
     if opt.env=="online" or opt.env=="offline" or opt.env=="test" or opt.env=="test1":
         kEnv=opt.env
     else:
@@ -88,7 +70,13 @@ def main():
         return
     else:
         kQid=opt.qid
+    if opt.type not in ('s125','s127','s128','s129','s130','p101','p104','p105','b116'):
+        print "Error Format type"
+        return
+    else:
+        kType=opt.type
     Report();
 
-main()
+if __name__=='__main__':
+    main()
 
